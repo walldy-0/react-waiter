36650ad4823bdfdedda1350e1f02b4f4857a41e3
Waldemar Szymon <walldy@tlen.pl>
2023-05-29 10:55:40 +0200

    Add initial data reading from api server

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ import Table from './components/pages/Table/Table';
 import NotFound from './components/pages/NotFound/NotFound';
 import Header from './components/views/Header/Header';
 import Footer from './components/views/Footer/Footer';
+import { fetchTables } from './redux/tablesRedux';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 
 function App() {
+  const dispatch = useDispatch();
+  
+  useEffect(() => dispatch(fetchTables()), [dispatch]);
+
   return (
     <Container>
       <Header />
